ce2c1376b07ad52ff16ce58fe6d77c80da6a8082
Yusuf karback <yusufkarback64@gmail.com>
2022-10-16 09:00:31 +0700

    routing register

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 application = Flask(__name__)
 
 @application.route('/')
-def index2():
+def index():
     return render_template('index2.html')
 
 @application.route('/dashboard')
@@ -13,6 +13,10 @@ def dashboard():
 @application.route('/login')
 def login():
     return render_template('login.html')
+
+@application.route('/register')
+def register():
+    return render_template('registration.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
